c8604e8f2b08225c7702a5443ea81631882ea611
William <w.f.l@hotmail.com>
2020-10-20 22:27:47 -0300

    create_equivalence_class

--- tests/AvaliadorTest.php
+++ tests/AvaliadorTest.php
@@ -45,4 +45,29 @@ class AvaliadorTest extends TestCase
 
         self::assertEquals(1000, $valor);
     }
+
+    public function testObterMaioresLances()
+    {
+        $leilao = new Leilao("Parati 1998 50km");
+
+        $jacy = new Usuario('Jacy');
+        $will = new Usuario("Will");
+        $wal = new Usuario("Wal");
+        $dani = new Usuario("Dani");
+
+        $leilao->recebeLance(new Lance($jacy, 1500));
+        $leilao->recebeLance(new Lance($will, 2000));
+        $leilao->recebeLance(new Lance($wal, 2700));
+        $leilao->recebeLance(new Lance($dani, 2300));
+
+        $leiloeiro = new Avaliador();
+
+        $leiloeiro->avalia($leilao);
+        $maiores = $leiloeiro->getMaioresLances();
+
+        static::assertCount(3, $maiores);
+        static::assertEquals(2700, $maiores[0]->getValor());
+        static::assertEquals(2300, $maiores[1]->getValor());
+        static::assertEquals(2000, $maiores[2]->getValor());
+    }
 }
